2d043908314bed97a9bddddaf7461bb4675b1c69
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-21 00:32:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,3 +21,12 @@ class Song
     @@all
   end
 end
+
+
+jay_z = Artist.new("Jay-Z")
+rap = Genre.new("rap")
+ninety_nine_problems = Song.new("99 Problems", rap)
+jay_z.add_song(ninety_nine_problems)
+another_crappy_song = Song.new("Crappy Song", rap)
+jay_z.add_song(another_crappy_song)
+jay_z.songs
